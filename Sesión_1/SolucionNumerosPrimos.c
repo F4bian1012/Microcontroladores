b06abff7344db434583068a635b6d7f506669149
F4bian1012 <jf.villavisan@uniandes.edu.co>
2025-02-21 10:11:06 -0500

    Solucion Numeros Primos

---
+++ Sesión_1/SolucionNumerosPrimos.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int numPrimo(int numP);
+
+int main(){
+    int numero=0;
+    scanf("%d", &numero);
+    printf("\n \n");
+    for (int i=2; i<numero; i++){
+        int y = numPrimo(i);
+        if (y==0){
+            printf("%d \n", i);
+        }
+    }
+    return 0;
+}
+
+
+int numPrimo( int numP){
+    int primo=0;
+    for (int i=2; i<numP; i++){
+        if (numP%i==0){
+            primo=1;
+            break;
+        }
+    }
+    return primo;
+}
